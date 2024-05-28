4325539d643fc21930715ccd96307ad3ce81042d
Oguzhan Telli <78385872+OguzhanTelli35@users.noreply.github.com>
2024-05-28 21:47:38 +0300

    1

--- substancesafe_beta/lib/main.dart
+++ substancesafe_beta/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:substancesafe_beta/screens/homepage.dart';
 import 'package:substancesafe_beta/screens/loginpage.dart';
