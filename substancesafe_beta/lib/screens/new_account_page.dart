0ba184c70a29db67657f9a09d306e0ef76ea93a2
Thomas <thomasjahreis3@gmail.com>
2024-06-07 15:57:26 +0200

    Added comments

--- substancesafe_beta/lib/screens/new_account_page.dart
+++ substancesafe_beta/lib/screens/new_account_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:substancesafe_beta/models/doctor.dart';
 import 'package:substancesafe_beta/models/patient.dart';
 import 'package:substancesafe_beta/screens/homepage.dart';
-
 
 class NewAccountPage extends StatelessWidget {
   @override
@@ -28,14 +28,24 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
   String _email = '';
   // ignore: unused_field
   String _password = '';
+  bool doc = false;
 
   void _submit() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Patient(email: _email,name: _name,password: _password);
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Account created for $_name')));
+      if (doc) {
+        Doctor(email: _email, name: _name, password: _password);
+      } else {
+        Patient(email: _email, name: _name, password: _password);
+      }
+
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Account created for $_name')));
     }
   }
+  final List<String> _options = ['Doctor', 'Patient',];
+  // Variable to hold the selected option
+  String? _selectedOption;
 
   @override
   Widget build(BuildContext context) {
@@ -93,14 +103,44 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
                 },
               ),
               SizedBox(height: 20),
+              Column(
+          children: _options.map((option) {
+            return ListTile(
+              title: Text(option),
+              leading: Radio<String>(
+                value: option,
+                groupValue: _selectedOption,
+                onChanged: (String? value) {
+                  setState(() {
+                    _selectedOption = value;
+                  });
+                },
+              ),
+            );
+          }).toList(),
+        ),
+        SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {
-            // Navigate to NewPage and remove CurrentPage from the stack
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => HomePage()),
+          // Perform an action with the selected option
+          if (_selectedOption != null) {
+            if(_selectedOption=='Doctor'){
+              doc=true;
+            }
+            else{doc=false;}
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('No role selected')),
             );
-          },
+          }
+                  _submit();
+                  // Navigate to NewPage and remove CurrentPage from the stack
+                  //After the PatientPage is created there will be and if depending on which account was created which will route to the correct page
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomePage()),
+                  );
+                },
                 child: Text('Create Account'),
               ),
             ],
